e259b7b22f6f44d84a356517fbe08e9caf116c27
valikm5 <116544714+valikm5@users.noreply.github.com>
2024-01-21 13:51:03 +0100

    new update!
    - Changed video quality for high and low for a while
    - Progress bar added
    - Few more changes...

--- app.py
+++ app.py
@@ -57,6 +57,14 @@ class App(customtkinter.CTk):
         self.video_quality_combobox = None
         self.qualitimenu = customtkinter.StringVar(value="")
 
+        # Progress bar
+        self.progress_label = customtkinter.CTkLabel(self, text="0%")
+        
+        self.progress_bar = customtkinter.CTkProgressBar(self, width=400)
+
+        # Create the status label
+        self.status = customtkinter.CTkLabel(self, text="", width=120, height=25)
+
     def download_btn(self):
         url = self.url_entry.get()
         
@@ -88,8 +96,7 @@ class App(customtkinter.CTk):
             # Create the combobox
             self.video_quality_combobox = customtkinter.CTkComboBox(
                 self,
-                values=["2160p", "1440p", "1080p", "720p",
-                         "480p", "360p", "240p", "144p"],
+                values=["High", "Low"],
                 variable=self.qualitimenu
             )
             self.video_quality_combobox.place(relx=0.4, rely=0.5, anchor=tk.CENTER)
@@ -99,41 +106,6 @@ class App(customtkinter.CTk):
             # Destroy combobox
             self.video_quality_combobox.destroy()
             self.video_quality_combobox = None
-
-    def download_video_btn(self, url):
-        download_label = customtkinter.CTkLabel(self, text="", width=120, height=25)
-        download_label.place(relx=0.5, rely=0.65, anchor=tk.CENTER) 
-        
-        try:
-            if self.video_quality_combobox is not None:
-                video_quality = self.video_quality_combobox.get()
-
-                """Processing .mp4 download"""
-                yt = YouTube(url)
-
-                # Default stream value (you might want to set it to a default)
-                stream = None
-
-                if video_quality == "2160p":
-                    stream = yt.streams.filter(res="2160p").first()
-                elif video_quality == "1440p":
-                    stream = yt.streams.filter(res="1440p").first()
-                elif video_quality == "1080p":
-                    stream = yt.streams.filter(res="1080p").first()
-                elif video_quality == "720p":
-                    stream = yt.streams.filter(res="720p").first()
-                elif video_quality == "480p":
-                    stream = yt.streams.filter(res="480p").first()
-                elif video_quality == "360p":
-                    stream = yt.streams.filter(res="360p").first()
-                
-                if stream:
-                    stream.download()
-                    download_label.configure(text="Downloaded!")
-                else:
-                    download_label.configure(text="No available resolution!")
-        except Exception as e:
-            raise e
 
     def download_audio_btn(self, url):
         try:
@@ -149,6 +121,49 @@ class App(customtkinter.CTk):
         except Exception as e:
             print(f"An unexpected error occured: {e}")
 
+    def download_video_btn(self, url):        
+        
+        # Show status
+        self.status.place(relx=0.5, rely=0.65, anchor=tk.CENTER)
+        
+        try:
+            if self.video_quality_combobox is not None:
+                video_quality = self.video_quality_combobox.get()
+
+                """Processing .mp4 download"""
+                yt = YouTube(url, on_progress_callback=self.on_progress)
+
+                if video_quality == "High":
+                    stream = yt.streams.get_highest_resolution()
+
+                elif video_quality == "Low":
+                    stream = yt.streams.get_lowest_resolution()
+
+                # Download video by quality
+                if stream:
+                    # Show progressbar and status
+                    self.progress_label.place(relx=0.5, rely=0.69, anchor=tk.CENTER)
+                    self.progress_bar.place(relx=0.5, rely=0.75, anchor=tk.CENTER)
+
+                    stream.download()
+                    # Update status text to "Downloaded!" with green color
+                    self.status.configure(text="Downloaded!", text_color="green")
+        except Exception as e:
+            # Update status text to the error message with red color
+            self.status.configure(text=str(e), text_color="red")
+
+    def on_progress(self, stream, chunk, bytes_remaining):
+        # Calculation
+        total_size = stream.filesize
+        bytes_downloaded = total_size - bytes_remaining
+        precentage_completed = bytes_downloaded / total_size * 100
+        
+        # Visual updating
+        self.progress_label.configure(text=str(int(precentage_completed)) + "%")
+        self.progress_bar.update()
+        self.progress_bar.set(float(precentage_completed / 100))
+
+        
 
 
 if __name__ == "__main__":
